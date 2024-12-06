97a38e701be3651f312e67d2add1d2131b6ffebd
satukauppinen <satu.kauppinen@centria.fi>
2024-12-06 14:47:28 +0200

    this is a new file

---
+++ routes/reservation.js
@@ -0,0 +1,25 @@
+import client from "../db/db.js";
+
+export async function registerReservation(c) {
+    const { pseudonym, resourceId, startTime, endTime } = await c.req.json();
+    try {
+        const query = `INSERT INTO abc123_reservations (pseudonym, resource_id, start_time, end_time) VALUES ($1, $2, $3, $4)`;
+        await client.queryArray(query, [pseudonym, resourceId, startTime, endTime]);
+        return c.text('Reservation added successfully');
+    } catch (error) {
+        console.error('Error adding reservation:', error);
+        return c.text('Error during adding reservation', 500);
+    }
+}
+
+export async function getReservations(c) {
+    const query = `SELECT resource_id, start_time, end_time FROM abc123_public_reservations`;
+    try {
+        const result = await client.queryObject(query);
+        console.log('Reservations fetched from DB:', result.rows);
+        return c.json(result.rows);
+    } catch (error) {
+        console.error('Error fetching reservations:', error);
+        return c.json({ error: 'Internal Server Error' }, 500);
+    }
+}
